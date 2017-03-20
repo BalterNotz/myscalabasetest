c4827ba94a93408f8d48bcc830e44de9e81f759b
zhangsongwei <zhangsongwei@jk.cn>
2017-03-20 21:33:33 +0800

    temp commit

---
+++ src/main/java/net/btnz/myscalabasetest/App.java
@@ -0,0 +1,13 @@
+package net.btnz.myscalabasetest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
